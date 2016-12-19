69ecbae9f3178d80512c956e39ba9425ce263a3f
Tim Coulter <tim@timothyjcoulter.com>
2016-12-19 10:53:29 -0800

    Pull running a process into its own Helper

---
+++ TruffleVSIX/Helpers/ProcessRunner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace TruffleVSIX.Helpers
+{
+    class ProcessRunner
+    {
+        public delegate void LineHandler(string line);
+        public event LineHandler OnLine;
+
+        private BackgroundWorker worker;
+        private LineHandler handler;
+
+        public ProcessRunner()
+        {
+            this.worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+        }
+
+        public void run(string command)
+        {
+            worker.RunWorkerAsync(command);
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/C " + (string)e.Argument;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.OutputDataReceived += (s, args) => {
+                this.OnLine(args.Data + "\r\n");
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+        }
+
+
+
+
+    }
+}
--- TruffleVSIX/ToolWindow1Control.xaml.cs
+++ TruffleVSIX/ToolWindow1Control.xaml.cs
@@ -9,9 +9,7 @@ namespace TruffleVSIX
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
-    using System.ComponentModel;
-    using System.Text;
-    using System.Diagnostics;
+    using Helpers;
 
     /// <summary>
     /// Interaction logic for ToolWindow1Control.
@@ -25,58 +23,16 @@ namespace TruffleVSIX
         {
             this.InitializeComponent();
 
-            //System.Timers.Timer timer = new System.Timers.Timer(100);
+            ProcessRunner runner = new ProcessRunner();
 
-            // Hook up the Elapsed event for the timer.
-            //timer.Elapsed += OnTimedEvent;
+            runner.OnLine += (line) =>
+            {
+                this.addText(line);
+            };
 
-            //timer.Enabled = true;
-
-
-            BackgroundWorker worker = new BackgroundWorker();
-
-            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
-            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
-
-            worker.RunWorkerAsync("8.8.8.8");
+            runner.run("pwd");
         }
-
-        private void worker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            //StringBuilder result = new StringBuilder();
-            //Process process = new Process();
-            //process.StartInfo.FileName = "ping";
-            //process.StartInfo.Arguments = (string)e.Argument;
-            //process.StartInfo.UseShellExecute = false;
-            //process.StartInfo.RedirectStandardOutput = true;
-            //process.StartInfo.CreateNoWindow = true;
-            //process.Start();
-            //result.Append(process.StandardOutput.ReadToEnd());
-            //process.WaitForExit();
-            //e.Result = result.AppendLine().ToString();
-
-            Process process = new Process();
-            process.StartInfo.FileName = "cmd.exe";
-            process.StartInfo.Arguments = "/C ping 8.8.8.8";
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.CreateNoWindow = true;
-            process.OutputDataReceived += (s, args) => this.addText(args.Data + "\r\n");
-            process.Start();
-            process.BeginOutputReadLine();
-        }
-
-        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            string str = "";
-
-            if (e.Result != null) str = e.Result.ToString();
-            else if (e.Error != null) str = e.Error.ToString();
-            else if (e.Cancelled) str = "User cancelled process";
-
-            this.addText(str);
-        }
-
+        
         private void addText(string str)
         {
             if (!Dispatcher.CheckAccess())
@@ -99,14 +55,6 @@ namespace TruffleVSIX
                     // This would fire when VS is closed. Not sure what to do here.
                 }
             }
-        }
-
-
-        private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs args)
-        {
-           // string str = "Bacon ipsum dolor amet swine alcatra venison bacon shank shankle pastrami rump jerky ball tip short loin kielbasa filet mignon pork chop.\r\n";
-
-            
         }
     }
 
