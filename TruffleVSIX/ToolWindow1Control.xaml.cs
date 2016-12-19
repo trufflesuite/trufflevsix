8b8fc08eb7ba356d20d2ad8f0b07d17c255917ed
Tim Coulter <tim@timothyjcoulter.com>
2016-12-19 10:21:42 -0800

    Run ping when the tool window starts, which shows I can get a process to run

--- TruffleVSIX/ToolWindow1Control.xaml.cs
+++ TruffleVSIX/ToolWindow1Control.xaml.cs
@@ -9,9 +9,9 @@ namespace TruffleVSIX
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
-
-    using Microsoft.VisualStudio.Shell;
-    using Microsoft.VisualStudio.Shell.Interop;
+    using System.ComponentModel;
+    using System.Text;
+    using System.Diagnostics;
 
     /// <summary>
     /// Interaction logic for ToolWindow1Control.
@@ -25,18 +25,60 @@ namespace TruffleVSIX
         {
             this.InitializeComponent();
 
-            System.Timers.Timer timer = new System.Timers.Timer(100);
+            //System.Timers.Timer timer = new System.Timers.Timer(100);
 
             // Hook up the Elapsed event for the timer.
-            timer.Elapsed += OnTimedEvent;
+            //timer.Elapsed += OnTimedEvent;
 
-            timer.Enabled = true;
+            //timer.Enabled = true;
+
+
+            BackgroundWorker worker = new BackgroundWorker();
+
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+
+            worker.RunWorkerAsync("8.8.8.8");
         }
 
-        private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs args)
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            string str = "Bacon ipsum dolor amet swine alcatra venison bacon shank shankle pastrami rump jerky ball tip short loin kielbasa filet mignon pork chop.\r\n";
+            //StringBuilder result = new StringBuilder();
+            //Process process = new Process();
+            //process.StartInfo.FileName = "ping";
+            //process.StartInfo.Arguments = (string)e.Argument;
+            //process.StartInfo.UseShellExecute = false;
+            //process.StartInfo.RedirectStandardOutput = true;
+            //process.StartInfo.CreateNoWindow = true;
+            //process.Start();
+            //result.Append(process.StandardOutput.ReadToEnd());
+            //process.WaitForExit();
+            //e.Result = result.AppendLine().ToString();
 
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/C ping 8.8.8.8";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.OutputDataReceived += (s, args) => this.addText(args.Data + "\r\n");
+            process.Start();
+            process.BeginOutputReadLine();
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            string str = "";
+
+            if (e.Result != null) str = e.Result.ToString();
+            else if (e.Error != null) str = e.Error.ToString();
+            else if (e.Cancelled) str = "User cancelled process";
+
+            this.addText(str);
+        }
+
+        private void addText(string str)
+        {
             if (!Dispatcher.CheckAccess())
             {
                 try
@@ -45,17 +87,26 @@ namespace TruffleVSIX
                     {
                         this.textBox.AppendText(str);
 
-                        // Magic number; just so users can attach it to the bottom again.
+                        // Magic number 20; just so users can attach it to the bottom again.
                         if (this.scrollViewer.VerticalOffset > this.scrollViewer.ScrollableHeight - 20)
                         {
                             this.scrollViewer.ScrollToBottom();
                         }
                     }, System.Windows.Threading.DispatcherPriority.Input);
-                } catch
+                }
+                catch
                 {
                     // This would fire when VS is closed. Not sure what to do here.
                 }
             }
+        }
+
+
+        private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs args)
+        {
+           // string str = "Bacon ipsum dolor amet swine alcatra venison bacon shank shankle pastrami rump jerky ball tip short loin kielbasa filet mignon pork chop.\r\n";
+
+            
         }
     }
 
